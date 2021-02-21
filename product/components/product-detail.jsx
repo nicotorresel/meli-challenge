a87bf9f04744162806a6641e644fd044b6e99520
nicotorresel <nicotorresel@gmail.com>
2021-02-21 18:37:10 -0300

    quantity and condition added to desc

--- product/components/product-detail.jsx
+++ product/components/product-detail.jsx
@@ -28,11 +28,15 @@ import React from "react";
 // tenia el componente escrito con clases y lo pase a componentes funcionales
 const ProductDetail = ({ product }) => {
 
-  const { image, alt, title, price, currency, description } = product;
+  const { image, alt, title, price, currency, description, condition, sold_quantity } = product;
 
   const moneda = {
     style: "currency",
     currency: currency
+  }
+
+  const condicion = () => {
+    return condition==="new"? "nuevo" : "usado";
   }
 
   return (
@@ -40,6 +44,7 @@ const ProductDetail = ({ product }) => {
       <div className="main">
         <img src={image} alt={alt}/>
         <div className="description">
+          <p>{condicion()} - {sold_quantity} vendidos</p>
           <h1>{title}</h1>
           <h2>{price.toLocaleString('es-AR', moneda)}</h2>
           <button>Comprar</button>
@@ -47,7 +52,7 @@ const ProductDetail = ({ product }) => {
       </div>
       <div className="description__more">
         <h3>Descripción del producto</h3>
-        <p>{description}</p>
+        <p>{title}</p>
       </div>
     </React.Fragment>
   );
