67b0e01cda93873f4e1c7d1d8cb48a037de0b5a2
nicotorresel <nicotorresel@gmail.com>
2021-02-22 23:50:08 -0300

    commented lines deleted

--- product/components/product-card.jsx
+++ product/components/product-card.jsx
@@ -2,34 +2,6 @@ import React from "react";
 
 // en principio realicé el componente con clases luego lo transforme a uno funcional
 
-// class ProductCard extends React.Component {
-//   render() {
-
-//     const { product } = this.props;
-//     const { image, alt, price, title, location } = product;
-
-//     return (
-//       <React.Fragment>
-//         <div className="card">
-//           <div className="card__detail">
-//             <div className="card__detail-image"></div>
-//             <img src={image} alt={alt}/>
-//             <div className="card__detail-description">
-//               <p className="price">{price.toLocaleString('es-AR', {style: 'currency', currency: 'ARS'})}</p>
-//               <p className="desc>"> {title}</p>
-//             </div>
-
-//           </div>
-//           <p className="location" >{location}</p>
-//         </div>
-//         <hr />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
-//paso el componente a uno funcional:
-
 const ProductCard = ( {product} ) => {
 
   const { image, alt, price, currency, title, location, condition, shipping } = product;
